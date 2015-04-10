7c1db8c151dafe75e6b4a532775926c97d666c74
kkapitan <kkapitan@MacBook-Pro-Krzysztof.local>
2015-04-10 14:59:06 +0200

    initial commit

---
+++ IntelliGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntelliGuide
+//
+//  Created by Krzysztof Kapitan on 10.04.2015.
+//  Copyright (c) 2015 Krzysztof Kapitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntelliGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntelliGuide
+//
+//  Created by Krzysztof Kapitan on 10.04.2015.
+//  Copyright (c) 2015 Krzysztof Kapitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
