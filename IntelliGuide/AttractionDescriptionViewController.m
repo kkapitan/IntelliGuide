598a4895f685c33507f46be14b7b806abb518157
Krystian Paszek <krystianpaszek93@gmail.com>
2015-04-19 13:22:24 +0200

    Interface for Attraction Details screen

---
+++ IntelliGuide/AttractionDescriptionViewController.m
@@ -0,0 +1,37 @@
+//
+//  AttractionDescriptionViewController.m
+//  IntelliGuide
+//
+//  Created by Krystian Paszek on 19.04.2015.
+//  Copyright (c) 2015 Krzysztof Kapitan. All rights reserved.
+//
+
+#import "AttractionDescriptionViewController.h"
+
+@interface AttractionDescriptionViewController ()
+
+@end
+
+@implementation AttractionDescriptionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
