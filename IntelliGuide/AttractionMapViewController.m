ec3a0cdc6dc77a9691d662378f6bde19bfefb090
Krystian Paszek <krystianpaszek93@gmail.com>
2015-06-14 11:30:03 +0200

    Can now click on pin bubble to see attraction details

--- IntelliGuide/AttractionMapViewController.m
+++ IntelliGuide/AttractionMapViewController.m
@@ -8,10 +8,11 @@
 
 #import "AttractionMapViewController.h"
 #import "IGAttraction.h"
+#import "AttractionDetailsViewController.h"
 
 @interface AttractionMapViewController () <MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-@property NSMutableArray *annotations;
+@property IGAttraction *destinationAttraction;
 @end
 
 @implementation AttractionMapViewController
@@ -21,9 +22,8 @@
     // Do any additional setup after loading the view.
     
     self.mapView.delegate = self;
-    self.annotations = [[NSMutableArray alloc] initWithCapacity:_attractions.count];
     [self addAnnotationsForAttractions:_attractions];
-    [self.mapView showAnnotations:_annotations animated:YES];
+    [self.mapView showAnnotations:self.mapView.annotations animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,14 +32,41 @@
 }
 
 - (void) addAnnotationsForAttractions:(NSArray*)attractions {
-    for (PFObject *attraction in self.attractions) {
+    for (PFObject *attraction in [self.attractions reverseObjectEnumerator]) {
         MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
         IGAttraction *igattraction = [IGAttraction attractionWithParseObject:attraction];
         annotation.coordinate = igattraction.location.coordinate;
-        [_annotations addObject:annotation];
+        annotation.title = igattraction.name;
+        [self.mapView addAnnotation:annotation];
     }
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Pin"];
+    pin.pinColor = MKPinAnnotationColorPurple;
+    pin.animatesDrop = YES;
+    pin.canShowCallout = YES;
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    return pin;
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    MKPointAnnotation *a = (MKPointAnnotation*)view.annotation;
+    NSInteger i = [self.attractions indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+        PFObject *o = (PFObject*)obj;
+        return o[[IGAttraction stringForKey:IGAttractionKeyName]] == a.title;
+    }];
+    self.destinationAttraction = [IGAttraction attractionWithParseObject:[self.attractions objectAtIndex:i]];
     
-    [self.mapView addAnnotations:_annotations];
+    [self performSegueWithIdentifier:@"FromMapToDetails" sender:self];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"FromMapToDetails"]) {
+        AttractionDetailsViewController *destVC = segue.destinationViewController;
+        destVC.attraction = self.destinationAttraction;
+    }
 }
 
 @end
